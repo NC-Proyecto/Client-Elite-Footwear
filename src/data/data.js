34410000e480d45b1c3154ff5c2420b01fb5db3b
FedericoG2 <fg0170700@gmail.com>
2024-02-21 22:43:57 -0300

    Templete catalogo de productos

--- src/data/data.js
+++ src/data/data.js
@@ -1,130 +1,82 @@
 export const data = [
   {
     id: 1,
-    name: "Double Cheeseburger",
-    category: "burger",
+    name: "Nike KD 10",
+    category: "Nike",
     image:
-      "https://images.unsplash.com/photo-1607013251379-e6eecfffe234?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YnVyZ2Vyc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=1400&q=60",
+      "https://i.pinimg.com/736x/56/b3/29/56b329f7757d276df1637dcf69a4e98d.jpg",
     price: "$$$$",
   },
   {
     id: 2,
-    name: "Bacon Cheeseburger",
-    category: "burger",
+    name: "NIKE KOBE A.D. 12 ",
+    category: "Nike",
     image:
-      "https://images.unsplash.com/photo-1553979459-d2229ba7433b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fGJ1cmdlcnN8ZW58MHx8MHx8&auto=format&fit=crop&w=1400&q=60",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKwxchKyINOofTP-oARndD3Y2oj9O6pTANa6i9qDQGF74_X03VpK8ryMmg-_I3ItPmbok&usqp=CAU",
     price: "$",
   },
   {
     id: 3,
-    name: "Mushroom Burger",
-    category: "burger",
+    name: "Underarmour 1",
+    category: "Underarmour",
     image:
-      "https://images.unsplash.com/photo-1608767221051-2b9d18f35a2f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGJ1cmdlcnN8ZW58MHx8MHx8&auto=format&fit=crop&w=1400&q=60",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQKJKgH6R1L40PAK8h1bGAq9TV8JvH6l-_Dkp-rY3FjuJwiaJXqS3r5XLz4AEX-fdVlQAg&usqp=CAU",
     price: "$$",
   },
   {
     id: 4,
-    name: "Loaded Burger",
-    category: "burger",
+    name: "Underarmour 2",
+    category: "Underarmour",
     image:
-      "https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8YnVyZ2Vyc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=1400&q=60",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_XZo4UT72ntKz3H7PWZEzU12C1Fk1SFD7wA&usqp=CAU",
     price: "$$$",
   },
   {
     id: 5,
-    name: "Feta & Spinnach",
-    category: "pizza",
+    name: "Adidas 1",
+    category: "Adidas",
     image:
-      "https://images.unsplash.com/photo-1593560708920-61dd98c46a4e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8cGl6emF8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
+      "https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/bc810c8cca4b44f883f7abd70124fee9_9366/Pro_Boost_Mid_Basketball_Shoes_White_FW9517_01_standard.jpg",
     price: "$$",
   },
   {
     id: 6,
-    name: "Supreme Pizza",
-    category: "pizza",
+    name: "Adidas 2",
+    category: "Adidas",
     image:
-      "https://images.unsplash.com/photo-1604382355076-af4b0eb60143?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8cGl6emF8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRpdXrggsQi2v5U7J1okKkq5r9d8_TkNhAoCw&usqp=CAU",
     price: "$$$",
   },
   {
     id: 7,
-    name: "Meat Lovers",
-    category: "pizza",
+    name: "Puma 1",
+    category: "Puma",
     image:
-      "https://images.unsplash.com/photo-1628840042765-356cda07504e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fHBpenphfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmadAU4NrIENnoxkPnfSS6Dop9QB2REi40KCxOTxkfxzQHRPkS5RXlM_s3QtcRJUwLYQg&usqp=CAU",
     price: "$$$$",
   },
   {
     id: 8,
-    name: "Cheese Pizza",
-    category: "pizza",
+    name: "Puma 2",
+    category: "Puma",
     image:
-      "https://images.unsplash.com/photo-1548369937-47519962c11a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8Y2hlZXNlJTIwcGl6emF8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
+      "https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_300,h_300/global/376941/07/sv03/fnd/PNA/fmt/png/PUMA-x-LAMELO-BALL-MB.01-Lo-Men's-Basketball-Shoes",
     price: "$",
   },
   {
     id: 9,
-    name: "Kale Salad",
-    category: "salad",
+    name: "Peak 1",
+    category: "Peak",
     image:
-      "https://images.unsplash.com/photo-1515543237350-b3eea1ec8082?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8c2FsYWQlMjBjZWFzYXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2xYT68wyU1a8vmMP0xL2vOPYltFAesgDcLQ&usqp=CAU",
     price: "$$",
   },
   {
     id: 10,
-    name: "Ceasar Salad",
-    category: "salad",
+    name: "Peak 2",
+    category: "Peak",
     image:
-      "https://images.unsplash.com/photo-1546793665-c74683f339c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8c2FsYWQlMjBjZWFzYXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
-    price: "$$$",
-  },
-  {
-    id: 11,
-    name: "Loaded Salad",
-    category: "salad",
-    image:
-      "https://images.unsplash.com/photo-1540420773420-3366772f4999?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8c2FsYWR8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
-    price: "$$$$",
-  },
-  {
-    id: 12,
-    name: "Fruit Salad",
-    category: "salad",
-    image:
-      "https://images.unsplash.com/photo-1564093497595-593b96d80180?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8ZnJ1aXQlMjBzYWxhZHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",
-    price: "$",
-  },
-  {
-    id: 13,
-    name: "Wings",
-    category: "chicken",
-    image:
-      "https://images.unsplash.com/photo-1567620832903-9fc6debc209f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Y2hpY2tlbiUyMGZvb2R8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
-    price: "$$",
-  },
-  {
-    id: 14,
-    name: "Baked Chicken",
-    category: "chicken",
-    image:
-      "https://images.unsplash.com/photo-1594221708779-94832f4320d1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8Y2hpY2tlbiUyMGZvb2R8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
-    price: "$$$$",
-  },
-  {
-    id: 15,
-    name: "Chicken Tenders",
-    category: "chicken",
-    image:
-      "https://images.unsplash.com/photo-1562967914-608f82629710?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGNoaWNrZW4lMjBmb29kfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
-    price: "$",
-  },
-  {
-    id: 16,
-    name: "Chicken Kabob",
-    category: "chicken",
-    image:
-      "https://images.unsplash.com/photo-1603360946369-dc9bb6258143?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fGNoaWNrZW4lMjBmb29kfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQK4Blfa7qRYInjylhAYdTgLOEhr5l9Dxnt31ICBpRPmngAb4eL2Nj5iDoV3dNJaIVzZfc&usqp=CAU",
     price: "$$$",
   },
 ];
